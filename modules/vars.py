edb9ded29f931910e37a44322aa8263641112041
chouhary12 <choudharytest2@gmail.com>
2026-05-06 23:44:09 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,16 +5,16 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22480303"))
 API_HASH = environ.get("API_HASH", "99c931b6c1ae6f8c3c3e87da173fa424")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8260420940:AAH6fYs8MyxQzZ8zNve5Lfll4XRsw9HPi0E")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8783249147:AAH35j4k1FjuuXJ_BzGhvtnGqgLDYSaCfU8")
 
 OWNER = int(environ.get("OWNER", "8295147093"))
 CREDIT = environ.get("CREDIT", "𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8222519141').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8295147093').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8295147093').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
